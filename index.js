6708f3103b1635f36c9fcc9a284e99478ff5ebef
Sebastian Martinez <isaacsoccer235@gmail.com>
2023-06-04 11:58:54 -0500

    gettin some stuff done

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 fetch('http://localhost:3000/animals')
     .then((r) => r.json())
     .then(animalData => {
-        console.log(animalData)
+
+        animalData.forEach((animal) => {
+            console.log(animal)
+        })
+
+        function renderAnimal(animal) {
+            const container = document.createElement('span')
+            const image = document.createElement('img')
+            const secondHeader = document.getElementById('second-header')
+    
+            secondHeader.append(container)
+            container.append(image)
+            console.log(secondHeader)
+    
+            image.src = animal.image
+        }
+
+
     })
 
+   
